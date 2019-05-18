68008f5e56f15be061afb61b4dfae772b1aaea16
Matthew Allen <matthewallenunc@gmail.com>
2019-05-18 19:12:54 -0400

    Switch creation

--- liri.js
+++ liri.js
@@ -1,3 +1,25 @@
 require("dotenv").config();
+var axios = require("axios");
+var fs = require("fs");
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+
+var command = process.argv.slice(2,3);
+var medArray = process.argv.slice(3);
+var media = medArray.join(" ");
+
+function switchStuff(command, media) {
+    switch (command) {
+
+        case 'spotify-this-song':
+            spotifyThis(media); break;
+        case 'movie-this':
+            movieThis(media); break;
+        case 'concert-this':
+            concertThis(media); break;
+        case 'do-what-it-says':
+            doWhatItSays(); break;
+    }
+};
+
