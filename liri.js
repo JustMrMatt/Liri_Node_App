5a63a7ec4fcc2305de59c89c9ff6a6438a0589f6
Matthew Allen <matthewallenunc@gmail.com>
2019-05-18 21:39:27 -0400

    concert This and Do what it says

--- liri.js
+++ liri.js
@@ -6,14 +6,14 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 var command = process.argv.slice(2,3);
-var medArray = process.argv.slice(3);
-var media = medArray.join(" ");
+var medAr = process.argv.slice(3);
+var media = medAr.join(" ");
 
 function switchStuff(command, media) {
     switch (command) {
 
         case 'spotify-this-song':
-            spotifyThis(media); break;
+            spotifyThisSong(media); break;
         case 'movie-this':
             movieThis(media); break;
         case 'concert-this':
@@ -23,16 +23,15 @@ function switchStuff(command, media) {
     }
 };
 
- // Not currently functioning, unsure why //
-function spotifyThis(media) {
+function spotifyThisSong(media) {
     if (media == "") {
         media = "The Sign"
     }
 
     spotify
-        .search({ type: 'track', query: media, limit: 1 })
-        .then(function (response) {
-            var song = response.tracks.items[0];
+        .search({ type: 'track', query: media, limit: 1 }).then(
+        function (input) {
+            var song = input.tracks.items[0];
             if (song != undefined) {
                 console.log("Song: " + song.name);
                 console.log("Artist: " + song.artists.name);
@@ -52,23 +51,53 @@ function movieThis(media) {
         media = "Mr. Nobody"
     }
 
-    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + media, function (err, response, data) {
-        try {
-            var response = JSON.parse(data)
-            if (response.Title != undefined) {
-                console.log(response.Title);
-                console.log(response.Year);
-                console.log(response.Ratings[0].Value);
-            } else {
-                console.log("Movie Unavailable");
+    axios.get("http://www.omdbapi.com/?t="+media+"&y=&plot=short&apikey=trilogy").then(
+        function (input, data) {
+                var input = JSON.parse(data)
+                if (input.Title != undefined) {
+                    console.log("Title: " + input.Title);
+                    console.log("Year: " + input.Year);
+                    console.log("MPAA Rating: " + input.Ratings[0].Value);
+                    console.log("Plot: " + input.Plot);
+                    console.log("Cast: " + input.Actors);
+                } else {
+                    console.log("Movie Unavailable");
+                }
             }
-        }
-        catch (err) {
-            console.log("Movie Unavailable");
-        }
-    });
+    );
 }
 
+function concertThis(media) {
+    if (media == "") {
+        media = "Taylor Swift"
+    }
+    axios.get("https://rest.bandsintown.com/artists/" + media + "/events?app_id=codingbootcamp", function (input, data) {
+            var input = JSON.parse(data)
+            if (input.length != 0) {
+                console.log("Upcoming Concerts: " + media);
+                input.forEach(function (info) {
+                    console.log("Venue name: " + info.venue.name);
+                    if (info.venue.country == "United States") {
+                        console.log("City: " + info.venue.city + ", " + info.venue.region);
+                    } else {
+                        console.log("City: " + info.venue.city + ", " + info.venue.country);
+                    }
+                    console.log("Date: " + moment(info.datetime).format('MM/DD/YYYY'));
+                })
+            } else {
+                console.log("No concerts found.");
+            }
+        }
+    );
+}
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(error, input) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(input);
+})};
 
 
 switchStuff(command, media);
